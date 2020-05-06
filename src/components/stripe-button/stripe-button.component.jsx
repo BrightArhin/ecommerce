12976b2647b82a38d107a7c6bd6b1da8e29f8234
Bright <barhin49@gmail.com>
2020-05-06 15:19:51 +0000

    extracted environment variables

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from "react-stripe-checkout"
 
 const StripeCheckoutButton = ({price})=>{
     const priceForStripe = price*100
-    const publishableKey = 'pk_test_voJdSQARyylVgu0zBJ6mtPbi00NU81wBzJ'
+    const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY
     const onToken = token =>{
         console.log(token)
         alert('Payment successful')
--- src/firebase/firebase-utils.js
+++ src/firebase/firebase-utils.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyA9A_TJtuVh_KOpX86G-o4r2xI4ZPQT_Ks",
-    authDomain: "bright-s-ecommerce.firebaseapp.com",
-    databaseURL: "https://bright-s-ecommerce.firebaseio.com",
-    projectId: "bright-s-ecommerce",
-    storageBucket: "bright-s-ecommerce.appspot.com",
-    messagingSenderId: "680826186239",
-    appId: "1:680826186239:web:f5f476bd5313d7fb8e1fca",
-    measurementId: "G-WEFYG4YS8Z"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
 };
 
 firebase.initializeApp(config)
